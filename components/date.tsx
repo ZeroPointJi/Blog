f73d09aa33204e47e7461dab43278a4121260ac9
JLK <zerodoublepoint@gmail.com>
2024-04-25 17:01:12 +0800

    迁移至tailwind-nextjs-starter-blog

--- components/date.tsx
+++
@@ -1,6 +0,0 @@
-import { format } from 'date-fns';
-
-export default function Date({ dateString }: { dateString: string | undefined }) {
-  const date = dateString || '';
-  return <time dateTime={date}>{format(date, 'yyyy年MM月dd日')}</time>;
-}
--- pages/_app.tsx
+++
@@ -1,8 +0,0 @@
-import '../styles/global.css';
-import type { AppProps } from 'next/app';
-
-interface CustomPageProps {}
-
-export default function App({ Component, pageProps }: AppProps<CustomPageProps>) {
-  return <Component {...pageProps} />;
-}
