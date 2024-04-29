733c6368225630003fb7fe202f8a50ba3d7e6efd
JLK <zerodoublepoint@gmail.com>
2024-04-29 11:11:18 +0800

    添加giscus 支持评论功能

--- components/Comments.tsx
+++ components/Comments.tsx
@@ -5,18 +5,6 @@ import { useState } from 'react'
 import siteMetadata from '@/data/siteMetadata'
 
 export default function Comments({ slug }: { slug: string }) {
-  const [loadComments, setLoadComments] = useState(false)
-
-  if (!siteMetadata.comments?.provider) {
-    return null
-  }
-  return (
-    <>
-      {loadComments ? (
-        <CommentsComponent commentsConfig={siteMetadata.comments} slug={slug} />
-      ) : (
-        <button onClick={() => setLoadComments(true)}>加载评论</button>
-      )}
-    </>
-  )
+  if (!siteMetadata.comments) return null
+  return <CommentsComponent commentsConfig={siteMetadata.comments} slug={slug} />
 }
--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -61,20 +61,23 @@ const siteMetadata = {
       category: process.env.NEXT_PUBLIC_GISCUS_CATEGORY,
       categoryId: process.env.NEXT_PUBLIC_GISCUS_CATEGORY_ID,
       mapping: 'pathname', // supported options: pathname, url, title
+      strict: 0,
       reactions: '1', // Emoji reactions: 1 = enable / 0 = disable
       // Send discussion metadata periodically to the parent window: 1 = enable / 0 = disable
-      metadata: '0',
+      emitMetadata: '0',
+      inputPosition: 'top',
       // theme example: light, dark, dark_dimmed, dark_high_contrast
       // transparent_dark, preferred_color_scheme, custom
       theme: 'light',
       // theme when dark mode
-      darkTheme: 'transparent_dark',
+      // darkTheme: 'transparent_dark',
+      lang: 'zh-CN',
       // If the theme option above is set to 'custom`
       // please provide a link below to your custom theme css file.
       // example: https://giscus.app/themes/custom_example.css
       themeURL: '',
-      // This corresponds to the `data-lang="en"` in giscus's configurations
-      lang: 'zh',
+      loading: 'lazy',
+      crossorigin: 'anonymous',
     },
   },
   search: {
